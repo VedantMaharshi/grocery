82a64a25003f83a84efd80bb739d58d003157670
VedantMaharshi <vedantmaharshi@gmail.com>
2026-05-07 13:27:51 +0530

    changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,17 +5,18 @@ const Footer = () => {
     <footer className="py-8 px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
             <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
                 <div>
-                    <h2 className='font-semibold text-2xl md:text-3xl text-indigo-600'>GoGrocey</h2>                
-                    <p className="mt-6 text-sm">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br>
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    <h2 className='font-bold text-2xl md:text-3xl text-indigo-600'><a href="/">GoGrocey</a></h2>                
+                    <p className="mt-6 font-bold">
+                        Your trusted partner for daily essentials, We handpick every item to ensure only the best reaches your home.<br></br>
+                        Premium groceries & effortless shopping, We bring the supermarket to your screen and the harvest to your home.<br></br>
+                        Eat fresh. Live better. Shop GoGrocey.
                     </p>
                 </div>
                 <div className="flex-1 flex items-start md:justify-end gap-20">
                     <div>
                         <h2 className="font-semibold mb-5 text-gray-800">Company</h2>
-                        <ul className="text-sm space-y-2">
-                            <li><a href="#">Home</a></li>
+                        <ul className="text-sm space-y-2 font-bold">
+                            <li><a href="/">Home</a></li>
                             <li><a href="#">About us</a></li>
                             <li><a href="#">Contact us</a></li>
                             <li><a href="#">Privacy policy</a></li>
@@ -23,18 +24,18 @@ const Footer = () => {
                     </div>
                     <div>
                         <h2 className="font-semibold text-gray-800 mb-5">Subscribe to our Community</h2>
-                        <div className="text-sm space-y-2">
+                        <div className="text-sm space-y-2 font-bold">
                             <p>The latest news, articles, and resources, sent to your inbox weekly.</p>
                             <div className="flex items-center gap-2 pt-4">
-                                <input className="border border-gray-500/30 placeholder-gray-500 focus:ring-2 ring-indigo-600 outline-none w-full max-w-64 h-9 rounded px-2" type="email" placeholder="Enter your email" />
+                                <input className="border border-gray-500/30 placeholder-gray-500 focus:ring-2 ring-indigo-600 outline-none w-full max-w-40 h-9 rounded px-2" type="email" placeholder="Enter your email" />
                                 <button className="bg-blue-600 w-24 h-9 text-white rounded">Subscribe</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <p className="pt-4 text-center text-xs md:text-sm pb-5">
-                Copyright 2024 © <a href="https://prebuiltui.com">GoGrocey</a>. All Right Reserved.
+            <p className="pt-2 text-center text-xs md:text-sm pb-5 font-bold">
+                Copyright 2026 © <a href="/">GoGrocey</a>. All Right Reserved.
             </p>
         </footer>
   )
